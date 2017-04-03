615b904ea8b1c440fe6d1fd140bc19b3f593de8b
Javid Izadfar <javid.izadfar@gmail.com>
2017-04-03 20:26:21 +0430

    Improved Mobie feel

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,1 +1,18 @@
-// Add custom js here
+function twlv(t) {
+  if (t > 12) t = t - 12;
+  return t
+}
+
+function updateTime() {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes()
+
+  document.getElementById('mobileTime').innerHTML = twlv(hour) + ':' + minute;
+}
+
+docReady(function() {
+  updateTime();
+  setInterval(function(){ updateTime() }, 10000);
+
+});
