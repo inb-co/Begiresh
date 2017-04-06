dfb0bd02f23669aca4d51b02a7a1dc4b50625c22
Javid Izadfar <javid.izadfar@gmail.com>
2017-04-07 00:28:40 +0430

    link to latest release

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -21,3 +21,29 @@ docReady(function() {
   setInterval(function(){ updateTime() }, 10000);
 
 });
+
+function fars(value) {
+  const english_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const parsi_digits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+  value = value.replace('v.', '');
+
+  for(let i = 0; i < value.length; i++){
+    if( english_digits.indexOf(value[i]) >= 0 ){
+      value = value.split(value[i]).join(parsi_digits[english_digits.indexOf(value[i])]);
+    }
+  }
+
+  return value;
+}
+
+axios.get('https://api.github.com/repos/inb-co/begiresh/releases/latest')
+  .then(function(response){
+
+    const button = document.getElementById('download_button');
+
+    button.getElementsByTagName('small')[0].innerHTML = '(ن.' + fars(response.data.tag_name) + ')';
+    button.href = response.data.zipball_url;
+    button.title = 'دانلود نسخه‌ی ' + fars(response.data.tag_name) + ' بگیرش از گیتهاب';
+
+  });
