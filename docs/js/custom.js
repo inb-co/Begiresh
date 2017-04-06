6037f0f6043ea32535db10116288edb57a72274f
Javid Izadfar <javid.izadfar@gmail.com>
2017-04-06 08:06:55 +0430

    changed mobile time type

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -3,12 +3,17 @@ function twlv(t) {
   return t
 }
 
+function dbl(t) {
+  if ( t < 10 ) t = '0' + t;
+  return t
+}
+
 function updateTime() {
   const now = new Date();
   const hour = now.getHours();
   const minute = now.getMinutes()
 
-  document.getElementById('mobileTime').innerHTML = twlv(hour) + ':' + minute;
+  document.getElementById('mobileTime').innerHTML = twlv(hour) + ':' + dbl(minute);
 }
 
 docReady(function() {
